6591624d5c2fe193b678725e3f412ff593c096d1
codeweft <codeweft@gmail.com>
2015-02-12 16:53:18 +0800

    creating server to host blocked domains

---
+++ server/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install nodejs -y
---
+++ server/dev.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#!/bin/sh
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install nodejs -y
+npm install -g strongloop
